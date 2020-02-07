f27d7300a2d2fc85c249a92f38a4ba4423216ca0
Koen Vervloesem <koen@vervloesem.eu>
2020-02-07 21:05:07 +0100

    Refactor commands

--- rpi_mini_battery_display/__init__.py
+++ rpi_mini_battery_display/__init__.py
@@ -8,7 +8,7 @@ SPDX-License-Identifier: MIT
 from time import sleep
 
 import RPi.GPIO as GPIO
-from RPi.GPIO import HIGH, LOW
+from RPi.GPIO import HIGH, IN, LOW, OUT
 
 LEVEL_TAB = [
     0x00,  #
@@ -25,10 +25,17 @@ BRIGHT_DARKEST = 0
 BRIGHT_TYPICAL = 2
 BRIGHTEST = 7
 
-ADDR_AUTO = 0x40
-ADDR_FIXED = 0x44
-STARTADDR = 0xC0
+# Chip commands
+# Data commands
+ADDR_FIXED = 0x44  # Set fixed address mode
+# Display control commands
+DISPLAY_OFF = 0x80  # Set display off
+DISPLAY_ON = 0x88  # Set display on
+# Address commands
+ADDR_START = 0xC0  # Set address of the display register
 
+# The TM1651's maximum frequency is 500 kHz with a 50% duty cycle.
+# We take a conservative clock cycle here.
 TM1651_CYCLE = 0.000050  # 50 microseconds
 
 
@@ -69,7 +76,16 @@ class InvalidPinError(Exception):
 
 
 class BatteryDisplay:
-    """Class to control the TM1651 battery display."""
+    """Class to control the TM1651 battery display.
+
+    The TM1651 chip communicates using a two-line serial bus interface.
+
+    Pins SEG1 to SEG7 (2-8) are connected to segments 1 to 7 of the display.
+
+    From pins GRID4 to GRID1 (9-12), only GRID1 is connected to the display.
+
+    Pin K1 (for key input) is not connected.
+    """
 
     def __init__(self, clock_pin=24, data_pin=23):
         """Initialize the TM1651 battery display object.
@@ -86,8 +102,8 @@ class BatteryDisplay:
         self.data_pin = data_pin
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(clock_pin, GPIO.OUT)
-        GPIO.setup(data_pin, GPIO.OUT)
+        GPIO.setup(clock_pin, OUT)
+        GPIO.setup(data_pin, OUT)
 
         self.set_brightness(BRIGHT_TYPICAL)
         self.clear_display()
@@ -106,12 +122,18 @@ class BatteryDisplay:
         brightness should be an integer from 0 to 7."""
         if brightness not in range(8):
             raise InvalidBrightnessError(brightness)
-        self.command_display_control = 0x88 + brightness
+        self.brightness = brightness
+
+    def send_command(self, *data):
+        """Send a command and optional data to the TM1651."""
+        self.start()
+        for byte in data:
+            self.write_byte(byte)
+        self.stop()
 
     def clear_display(self):
         """Clear the display."""
         self.set_level(0)
-        self.frame(False)
 
     def set_level(self, level):
         """Display a level on the battery display.
@@ -120,41 +142,9 @@ class BatteryDisplay:
         if level not in range(8):
             raise InvalidLevelError(level)
 
-        # Start signal sent to TM1651
-        self.start()
-        self.write_byte(ADDR_FIXED)
-        self.stop()
-
-        self.start()
-        self.write_byte(0xC0)
-        self.write_byte(LEVEL_TAB[level])
-        self.stop()
-
-        self.start()
-        self.write_byte(self.command_display_control)
-        self.stop()
-
-    def frame(self, frame_on):
-        """Turn a frame on or off."""
-        if frame_on:
-            seg_data = 0x40
-        else:
-            seg_data = 0x00
-
-        # Start signal sent to TM1651
-        self.start()
-        self.write_byte(ADDR_AUTO)
-        self.stop()
-
-        self.start()
-        self.write_byte(0xC1)
-        for _ in range(3):
-            self.write_byte(seg_data)
-        self.stop()
-
-        self.start()
-        self.write_byte(self.command_display_control)
-        self.stop()
+        self.send_command(ADDR_FIXED)
+        self.send_command(ADDR_START, LEVEL_TAB[level])
+        self.send_command(DISPLAY_ON + self.brightness)
 
     def write_byte(self, write_data):
         """Write a byte to the TM1651."""
@@ -185,11 +175,11 @@ class BatteryDisplay:
 
         # CLK high, set DIO to input
         self.set_clock(HIGH)
-        GPIO.setup(self.data_pin, GPIO.IN)
+        GPIO.setup(self.data_pin, IN)
         sleep(TM1651_CYCLE)
 
         ack = GPIO.input(self.data_pin)
-        GPIO.setup(self.data_pin, GPIO.OUT)
+        GPIO.setup(self.data_pin, OUT)
 
         if not ack:
             self.set_data(LOW)
